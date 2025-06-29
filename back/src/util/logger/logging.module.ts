fc7b9e8b5e171776ec2c9595a406fcf020c894b9
ShinSungkyu <kxu4583@naver.com>
2025-06-29 21:38:11 +0900

    ✨ feat: 다형적 API 로거 플랫폼 구현
    - 기존엔 특정 API 요청 로거가 필요할 때마다 새로운 로거를 만들어 등록해야 하는 단점이 있었음.
    - API 요청 로거 간에 겹치는 기능을 모두 통합하고, 옵션 선택을 통해 사용처마다 로거를 쉽게 디자인하여 사용할 수 있도록 함.
    - `logger.module`에서 세부 로거를 정의하면, 사용처(controller)에서 `@UseInterceptor`를 붙여 사용 가능함.
    
    Issue Resolved: #319

--- back/src/util/logger/logging.module.ts
+++ back/src/util/logger/logging.module.ts
@@ -1,16 +1,42 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { WinstonModule } from 'nest-winston';
 
-import { DetailedRequestLoggingInterceptor } from './interceptor/detailed-request-logger.interceptor';
-import { LoggerMiddleware } from './middleware/default-request-logger.middleware';
+import { RequestLoggingInterceptor } from './interceptor/request-logging.interceptor';
+import { RequestLoggingMiddleware } from './middleware/request-logging.middleware';
+import { RequestLoggingService } from './service/request-logging.service';
+import { LoggingDetailType } from './types/logging.types';
+import { winstonConfig } from './winston.config';
+
+const DefaultRequestLoggingMiddleware = RequestLoggingMiddleware.instantiate({
+  includeDetails: [LoggingDetailType.RESPONSE_TIME, LoggingDetailType.STATUS_CODE],
+  loggerName: 'Default-API',
+});
+
+export const FullRequestLoggingInterceptor = RequestLoggingInterceptor.instantiate({
+  excludeDetails: [],
+  loggerName: 'Full-API',
+});
 
 @Module({
-  imports: [WinstonModule],
-  providers: [LoggerMiddleware, DetailedRequestLoggingInterceptor],
-  exports: [DetailedRequestLoggingInterceptor],
+  imports: [WinstonModule.forRoot(winstonConfig)],
+  providers: [
+    RequestLoggingService,
+    RequestLoggingInterceptor,
+    RequestLoggingMiddleware,
+
+    DefaultRequestLoggingMiddleware,
+    FullRequestLoggingInterceptor,
+  ],
+  exports: [
+    RequestLoggingService,
+    RequestLoggingInterceptor,
+    RequestLoggingMiddleware,
+
+    FullRequestLoggingInterceptor,
+  ],
 })
 export class LoggingModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('*');
+    consumer.apply(DefaultRequestLoggingMiddleware).forRoutes('*');
   }
 }
