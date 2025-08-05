35f74faeb056800ddb0d4c33cb5226c2174b4f21
ShinSungkyu <kxu4583@naver.com>
2025-08-05 17:14:19 +0900

    ✨ feat: 벤치마크 모듈에 로깅 모듈 적용
    Issue Resolved: #335

--- back/src/benchmark/benchmark.module.ts
+++ back/src/benchmark/benchmark.module.ts
@@ -4,13 +4,14 @@ import { EventEmitterModule } from '@nestjs/event-emitter';
 import { AuthModule } from '../auth/auth.module';
 import { BookingModule } from '../domains/booking/booking.module';
 import { EventModule } from '../domains/event/event.module';
+import { LoggingModule } from '../util/logger/logging.module';
 
 import { BenchmarkController } from './controller/benchmark.controller';
 import { SeatsGateway } from './gateway/seats.gateway';
 import { SeatsBenchmarkService } from './service/seats-benchmark.service';
 
 @Module({
-  imports: [EventEmitterModule.forRoot(), BookingModule, EventModule, AuthModule],
+  imports: [EventEmitterModule.forRoot(), BookingModule, EventModule, AuthModule, LoggingModule],
   controllers: [BenchmarkController],
   providers: [SeatsGateway, SeatsBenchmarkService],
   exports: [],
