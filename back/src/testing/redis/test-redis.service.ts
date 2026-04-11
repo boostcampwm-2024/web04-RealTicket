44175fbac809bf00f12cd43fe4f3e6565bae5af9
ShinSungkyu <kxu4583@naver.com>
2026-04-12 04:22:24 +0900

    🔧 chore: TestRedisService pubsub 클라이언트 분리

--- back/src/testing/redis/test-redis.service.ts
+++ back/src/testing/redis/test-redis.service.ts
@@ -5,12 +5,15 @@ import RedisMock from 'ioredis-mock';
 @Injectable()
 export class TestRedisService {
   private redisClient: Redis;
+  private pubsubClient: Redis;
 
   constructor() {
     this.redisClient = new RedisMock() as unknown as Redis;
+    this.pubsubClient = new RedisMock() as unknown as Redis;
   }
 
-  getOrThrow(): Redis {
+  getOrThrow(namespace?: string): Redis {
+    if (namespace === 'pubsub') return this.pubsubClient;
     return this.redisClient;
   }
 
@@ -20,5 +23,6 @@ export class TestRedisService {
 
   async disconnect(): Promise<void> {
     this.redisClient.disconnect();
+    this.pubsubClient.disconnect();
   }
 }
